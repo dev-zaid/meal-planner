43f6309c3b0ee3a350a0a275164ce72d8672a4f0
Mohd Zaid <zaid.tab123@gmail.com>
2022-08-03 12:01:03 +0530

    🐛 fix reference in schema

--- src/helper/models/schema.ts
+++ src/helper/models/schema.ts
@@ -15,28 +15,33 @@ enum mealCategory {
   DINNER = 'DINNER',
 }
 
-const user = {
-  name: string().required(),
-  calorieRequirement: string().required(),
-  mealPlan: array()
-    .of(object().shape({ date: date().required(), mealsReference: string().required() }))
-    .required(),
-};
-
-const meal = {
-  category: mixed<mealCategory>().oneOf(Object.values(mealCategory)).required(),
-  name: string().required(),
-  foodItems: array().required(),
-};
-
 const food = {
-  name: string().required(),
+  name: string().required('Food name is required'),
   calories: number().optional(),
   protein: number().optional(),
   carbs: number().optional(),
   fat: number().optional(),
-  acceptedUnits: array().of(mixed<unitsENum>().oneOf(Object.values(unitsENum)).required()),
-  itemWeight: string().required(),
+  acceptedUnits: array().of(mixed<unitsENum>().oneOf(Object.values(unitsENum)).required('Units is missing')),
+  itemWeight: string().required('Item weight/quantity is required'),
+};
+
+const meal = {
+  category: mixed<mealCategory>().oneOf(Object.values(mealCategory)).required('Category of meal is required'),
+  name: string().required('Name of meal is mandatory'),
+  foodItems: array().of(string().required('Food items is missing')).required(), //Array of food item names
+};
+
+const user = {
+  name: string().required('Name is mandatory'),
+  calorieRequirement: string().required('Calorie requirement is mandatory'),
+  mealPlan: array()
+    .of(
+      object().shape({
+        date: date().required('Date is required'),
+        mealsReference: string().required('Meal name is required'),
+      }),
+    ) //mealsReference refernces to the name of the meal in the meal collection
+    .required('Meal plan is mandatory'),
 };
 
 export const userSchema = new ObjectSchema(user);
